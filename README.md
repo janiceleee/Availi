f738a5474dc9f00efac53300e508906960a9489b
janiceleee <39648139+janiceleee@users.noreply.github.com>
2022-03-14 17:17:50 +0800

    Added bot commands
    Added !add, !delete, !show, !availi

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Install python library that implements discord api
 `pip install -U discord.py`
 
+Bot commands
+`!add`
+`!delete`
+`!show`
+`!availi`
+`!help`
--- availi.py
+++ availi.py
@@ -1,6 +1,5 @@
 import os
 
-import discord
 from discord.ext import commands
 
 TOKEN = 'OTUyODMxMjk3NTczNzY5MjM2.Yi7vTA.OZnBwjovLtQrzxZOW7EN6wW68nw'
@@ -19,11 +18,25 @@ async def on_member_join(member):
         f'Hi {member.name}, welcome to my Discord server!'
     )
 
-@bot.command(name='add', help='Add available time')
+@bot.command(name='add', help='Add available times')
 async def add_time(ctx, day, time):
     response = 'Added timing'
     await ctx.send(response)
 
-	
+@bot.command(name='delete', help='Delete specific available times')
+async def add_time(ctx, day, time):
+    response = 'Deleted timing'
+    await ctx.send(response)	
+
+@bot.command(name='show', help='Show user\'s available times')
+async def add_time(ctx):
+    response = 'todo: show available time'
+    await ctx.send(response)
+
+@bot.command(name='availi', help='Show available times')
+async def add_time(ctx):
+    response = 'todo: show available time'
+    await ctx.send(response)
+
 
 bot.run(TOKEN)
